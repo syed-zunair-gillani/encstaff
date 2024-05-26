8a9a7212b363091cb3c969bd026ae80dadb2f240
syed-zunair-gillani <zunairgillani54@gmail.com>
2024-05-26 18:05:06 +0500

    y

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-import axios, { endpoints } from 'src/utils/axios';
+// import axios, { endpoints } from 'src/utils/axios';
 
 import { ProductShopDetailsView } from 'src/sections/product/view';
 
@@ -20,10 +20,10 @@ export default function ProductShopDetailsPage({ params }: Props) {
   return <ProductShopDetailsView id={id} />;
 }
 
-export async function generateStaticParams() {
-  const res = await axios.get(endpoints.product.list);
+// export async function generateStaticParams() {
+//   const res = await axios.get(endpoints.product.list);
 
-  return res.data.products.map((product: { id: string }) => ({
-    id: product.id,
-  }));
-}
+//   return res.data.products.map((product: { id: string }) => ({
+//     id: product.id,
+//   }));
+// }
